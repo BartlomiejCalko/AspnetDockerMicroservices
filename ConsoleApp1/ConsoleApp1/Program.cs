c754e2aa674372a51c30b158a163a3b9647bbf84
Bartlomiej Calko <bartlomiej.calko@gmail.com>
2022-05-24 12:28:20 +0200

    context, repo, crud, controller

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			DateTime currentDateTime = DateTime.Now;
+			DateTime todaysDate = DateTime.Today;
+			DateTime currentDateTimeUTC = DateTime.UtcNow;
+
+			DateTime maxDateTimeValue = DateTime.MaxValue;
+			DateTime minDateTimeValue = DateTime.MinValue;
+
+			Console.WriteLine($"Current DateTime {currentDateTime.Day}");
+			Console.WriteLine($"Today's DateTime {todaysDate}");
+			Console.WriteLine($"Current DateTime UTC Timezone {currentDateTimeUTC}");
+			Console.WriteLine($"Max DateTime Value {maxDateTimeValue}");
+			Console.WriteLine($"Min DateTime Value {minDateTimeValue}");
+
+
+
+		}
+    }
+}
